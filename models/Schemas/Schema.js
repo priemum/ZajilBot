d627826d79a9eef992511d01d94c7b65aa6e65fa
priemum <50713962+priemum@users.noreply.github.com>
2021-10-15 15:02:57 +0200

    Update Schema.js

--- models/Schemas/Schema.js
+++ models/Schemas/Schema.js
@@ -1,6 +1,6 @@
 const { model, Schema } = require("mongoose");
 const schema = new Schema({
   Guild: String,
-  Channel: String,
+  Channel: String,       
 });
 module.exports = model("Zajil", schema);
